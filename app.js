7a7ab3f109fd6eec2b97bc6d4eae8f3a4f04bf50
lixiang2 <549573735@qq.com>
2017-10-19 11:34:58 +0800

    add server

---
+++ app.js
@@ -0,0 +1,16 @@
+ const http = require('http')
+ const hostname = '127.0.0.1';
+ const port =3006;
+
+ const server=http.createServer(function(req, res) {
+     res.writeHead(200, { 'Content-Type': 'text/plain' })
+     res.end('hello word')
+ })
+ server.listen(port,hostname,()=>{
+ 	console.log('server running on http://${hostname}:${port}')
+ })
+
+
+
+
+ 
