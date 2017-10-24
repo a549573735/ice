27c617878597d2ea5e5032e0325b5d70853a6772
lixiang2 <549573735@qq.com>
2017-10-24 11:37:26 +0800

    修改

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
  const server=http.createServer(function(req, res) {
      res.writeHead(200, { 'Content-Type': 'text/plain' })
-     res.end('hello word')
+     res.end('hello ice')
  })
  server.listen(port,hostname,()=>{
  	console.log(`server running on http://${hostname}:${port}`)
